104b533a55ca1b9f72c66184c7f1b300e87fc5c0
Mihálka Gábor (Gabi) <mihalkagabor@gmail.com>
2025-11-12 11:10:15 +0100

    Első indítás sikeres, flyway létre hozta a táblát. Compose.yaml létrehozta a db-t.

---
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users(
+    id BIGSERIAL PRIMARY KEY,
+    user_name VARCHAR(255) UNIQUE NOT NULL ,
+    password_hash VARCHAR(255) NOT NULL ,
+    role VARCHAR(255) NOT NULL
+);
