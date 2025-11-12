7110f18039a01b4b65258140bc0cc1920cd3463a
Mihálka Gábor (Gabi) <mihalkagabor@gmail.com>
2025-11-12 10:55:35 +0100

    Initial commit

---
+++ src/main/java/org/mihalka/workoutbackend/WorkoutbackendApplication.java
@@ -0,0 +1,13 @@
+package org.mihalka.workoutbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mihalka/workoutbackend/WorkoutbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mihalka.workoutbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
